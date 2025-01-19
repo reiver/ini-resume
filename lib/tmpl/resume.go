b7580931f6c7567fa3893e3e5765f7dcc6b3c3ec
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-19 05:25:04 -0800

    template

---
+++ lib/tmpl/resume.go
@@ -0,0 +1,9 @@
+package libtmpl
+
+type ResumeHTML struct {
+	Basics        BasicsHTML
+	Addresses   []AddressHTML
+	WorkHistory []WorkHTML
+	Educations  []EducationHTML
+	References  []ReferenceHTML
+}
